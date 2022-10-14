b36a9bbea4144d40b49f464704cae725ebd8caa7
code_ever97 <txt_zju@outlook.com>
2022-10-14 18:26:40 +0800

    Update compile.bat
    new config

--- compile.bat
+++ compile.bat
@@ -1,7 +1,8 @@
 @ECHO OFF
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
-SET VCPKG_DIR=C:\Users\txt_len\Documents\me\2021\projects\gameboyemulator\vcpkg
-SET CMAKE_EXE="C:\Program Files\CMake\bin\cmake.exe"
+SET VCPKG_DIR=D:\Source\Repos\darktxt\meGB\vcpkg
+SET CMAKE_EXE="D:\Program Files\CMake\bin\cmake.exe"
+SET Visual_Studio="Visual Studio 17 2022"
 SET VSCMD_ARG_TGT_ARCH=x64
 
 RMDIR /s/q build
@@ -10,6 +11,6 @@ PUSHD build
 
 %VCPKG_DIR%\vcpkg.exe install sdl2:%VSCMD_ARG_TGT_ARCH%-windows
 
-%CMAKE_EXE% .. "-DCMAKE_TOOLCHAIN_FILE=%VCPKG_DIR%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=%VSCMD_ARG_TGT_ARCH%-windows -G "Visual Studio 16 2019"
+%CMAKE_EXE% .. "-DCMAKE_TOOLCHAIN_FILE=%VCPKG_DIR%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=%VSCMD_ARG_TGT_ARCH%-windows -G %Visual_Studio%
 
 ENDLOCAL
