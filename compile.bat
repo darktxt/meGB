e9c7398f003e8a1088bc028702cf30447efabfb2
darktxt <txt_zju@outlook.com>
2022-10-14 18:45:52 +0800

    git test

--- compile.bat
+++ compile.bat
@@ -2,7 +2,7 @@
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 SET VCPKG_DIR=D:\Source\Repos\darktxt\meGB\vcpkg
 SET CMAKE_EXE="D:\Program Files\CMake\bin\cmake.exe"
-SET Visual_Studio="Visual Studio 17 2022"
+SET VISUAL_STUDIO_VERSION="Visual Studio 17 2022"
 SET VSCMD_ARG_TGT_ARCH=x64
 
 RMDIR /s/q build
@@ -11,6 +11,6 @@ PUSHD build
 
 %VCPKG_DIR%\vcpkg.exe install sdl2:%VSCMD_ARG_TGT_ARCH%-windows
 
-%CMAKE_EXE% .. "-DCMAKE_TOOLCHAIN_FILE=%VCPKG_DIR%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=%VSCMD_ARG_TGT_ARCH%-windows -G %Visual_Studio%
+%CMAKE_EXE% .. "-DCMAKE_TOOLCHAIN_FILE=%VCPKG_DIR%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=%VSCMD_ARG_TGT_ARCH%-windows -G %VISUAL_STUDIO_VERSION%
 
 ENDLOCAL
